024e60a487a2e03c3c300c901729e9912271c3e5
KoteiIto <mk.mdrm.cmtr@gmail.com>
2018-05-15 21:54:17 +0900

    update test

--- entity/entity.go
+++ entity/entity.go
@@ -1,15 +1,17 @@
 package entity
 
 type Ticker struct {
-	Sell float64 `json:"sell,string"`
-	Buy  float64 `json:"buy,string"`
-	High float64 `json:"high,string"`
-	Low  float64 `json:"low,string"`
-	Last float64 `json:"last,string"`
-	Vol  float64 `json:"vol,string"`
+	Sell      float64 `json:"sell,string"`
+	Buy       float64 `json:"buy,string"`
+	High      float64 `json:"high,string"`
+	Low       float64 `json:"low,string"`
+	Last      float64 `json:"last,string"`
+	Vol       float64 `json:"vol,string"`
+	Timestamp int     `json:"timestamp"`
 }
 
 type Depth struct {
-	Asks [][]float64 `json:"asks,string"`
-	Bids [][]float64 `json:"bids,string"`
+	Asks      [][]float64 `json:"asks,string"`
+	Bids      [][]float64 `json:"bids,string"`
+	Timestamp int         `json:"timestamp"`
 }
