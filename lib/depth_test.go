6011afc19ab0d082cff8292addacdf39b10dbd47
KoteiIto <mk.mdrm.cmtr@gmail.com>
2018-07-21 13:10:25 +0900

    fix test

---
+++ lib/depth_test.go
@@ -0,0 +1,32 @@
+package lib
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDepth(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	client := NewMockclient(ctrl)
+	client.EXPECT().request(&clientOption{
+		endpoint: publicApiEndpoint,
+		method:   http.MethodGet,
+		path:     fmt.Sprintf(formatDepth, PairBtcJpy),
+	}).Return(
+		[]byte(`{"success":1,"data":{"asks":[["964745","0.0004"],["964753","0.0010"]],"bids":[["964254","0.0060"],["964249","0.0200"]],"timestamp":1526387708186}}`),
+		nil,
+	)
+	api := &APIImpl{
+		client: client,
+	}
+	depth, err := api.GetDepth(PairBtcJpy)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, depth)
+	assert.Equal(t, []float64{964745.0, 0.0004}, depth.Asks[0])
+	assert.Equal(t, []float64{964254.0, 0.0060}, depth.Bids[0])
+}
---
+++ lib/ticker_test.go
@@ -0,0 +1,35 @@
+package lib
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTicker(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	client := NewMockclient(ctrl)
+	client.EXPECT().request(&clientOption{
+		endpoint: publicApiEndpoint,
+		method:   http.MethodGet,
+		path:     fmt.Sprintf(formatTicker, PairBtcJpy),
+	}).Return(
+		[]byte(`{"success":1,"data":{"sell":"1020979","buy":"1020712","high":"1023889","low":"963930","last":"1020984","vol":"2075.8257","timestamp":1524573765864}}`),
+		nil,
+	)
+	api := &APIImpl{
+		client: client,
+	}
+	ticker, err := api.GetTicker(PairBtcJpy)
+	assert.Nil(t, err)
+	assert.NotNil(t, ticker)
+	assert.Equal(t, 1020712.0, ticker.Buy)
+	assert.Equal(t, 1020979.0, ticker.Sell)
+	assert.Equal(t, 1023889.0, ticker.High)
+	assert.Equal(t, 963930.0, ticker.Low)
+	assert.Equal(t, 1020984.0, ticker.Last)
+	assert.Equal(t, 2075.8257, ticker.Vol)
+}
